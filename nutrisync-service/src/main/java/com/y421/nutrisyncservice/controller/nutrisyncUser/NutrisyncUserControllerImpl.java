b75662347223a0c23821765f015ec20a49bafbda
charinfernando <charinranditha@gmail.com>
2025-12-30 14:28:44 +0530

    frontend main branch created

--- nutrisync-service/src/main/java/com/y421/nutrisyncservice/controller/nutrisyncUser/NutrisyncUserControllerImpl.java
+++
@@ -1,4 +0,0 @@
-package com.y421.nutrisyncservice.controller.nutrisyncUser;
-
-public class NutrisyncUserControllerImpl implements NutrisyncUserController {
-}
--- nutrisync-service/src/main/java/com/y421/nutrisyncservice/service/nutrisyncUser/NutrisyncUserServiceImpl.java
+++
@@ -1,7 +0,0 @@
-package com.y421.nutrisyncservice.service.nutrisyncUser;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class NutrisyncUserServiceImpl implements NutrisyncUserService {
-}
