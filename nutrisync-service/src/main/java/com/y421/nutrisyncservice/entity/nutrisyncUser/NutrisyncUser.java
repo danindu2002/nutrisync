387a035791ccae23ebebc0ce8c826b86d62cae18
DilshanPeiris <dilshan.p@informaticsint.com>
2026-01-01 12:30:45 +0530

    NutrisyncUser entity implemented.

--- nutrisync-service/src/main/java/com/y421/nutrisyncservice/entity/nutrisyncUser/NutrisyncUser.java
+++ nutrisync-service/src/main/java/com/y421/nutrisyncservice/entity/nutrisyncUser/NutrisyncUser.java
@@ -8,6 +8,8 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
 
 @Entity
 @Getter
@@ -25,6 +27,42 @@ public class NutrisyncUser extends AuditModel implements Serializable {
 
     @Column(name = "FIRST_NAME", nullable = false)
     private String firstName;
+
+    @Column(name = "LAST_NAME", nullable = false)
+    private String lastName;
+
+    @Column(name = "PASSWORD", nullable = false)
+    private String password;
+
+    @Column(name = "EMAIL", nullable = false)
+    private String email;
+
+    @Column(name = "DATE_OF_BIRTH", nullable = false)
+    private Date dateOfBirth;
+
+    @Column(name = "GENDER", nullable = false)
+    private String gender;
+
+    @Column(name = "AGE", nullable = false)
+    private Integer age;
+
+    @Column(name = "HEIGHT", nullable = false)
+    private Float height;
+
+    @Column(name = "WEIGHT", nullable = false)
+    private Float weight;
+
+    @Column(name = "BMI", nullable = false)
+    private Float bmi;
+
+    @Column(name = "ACTIVITY_LEVEL", nullable = false)
+    private String activityLevel;
+
+    @Column(name = "DIETARY_PREFERENCES", nullable = false)
+    private List<String> dietaryPreferences;
+
+    @Column(name = "REG_DATE", nullable = false)
+    private Date regDate;// user registered date
 
     // todo: add other fields as necessary
 }
