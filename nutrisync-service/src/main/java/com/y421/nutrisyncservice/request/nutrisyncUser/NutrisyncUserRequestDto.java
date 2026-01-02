c1ba3e73280844d01e5fe382dc30d3d309e59172
DilshanPeiris <dilshan.p@informaticsint.com>
2026-01-02 09:18:27 +0530

    NutrisyncUser req DTO implemented.

--- nutrisync-service/src/main/java/com/y421/nutrisyncservice/request/nutrisyncUser/NutrisyncUserRequestDto.java
+++ nutrisync-service/src/main/java/com/y421/nutrisyncservice/request/nutrisyncUser/NutrisyncUserRequestDto.java
@@ -4,8 +4,24 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-//@Data
-//@AllArgsConstructor
-//@NoArgsConstructor
+import java.util.Date;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class NutrisyncUserRequestDto {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private Date dateOfBirth;
+    private String gender;
+    private Integer age;
+    private Float height;
+    private Float weight;
+    private Float bmi;
+    private String activityLevel;
+    private List<String> dietaryPreferences;
+    private Date regDate;
 }
