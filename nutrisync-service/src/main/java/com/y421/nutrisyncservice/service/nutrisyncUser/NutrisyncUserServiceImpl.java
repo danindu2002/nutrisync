5daf2575a174d5fcd2131360d93191b0fad7b603
DilshanPeiris <dilshan.p@informaticsint.com>
2026-01-07 12:59:39 +0530

    NutrisyncUserService method implementation.

--- nutrisync-service/src/main/java/com/y421/nutrisyncservice/service/nutrisyncUser/NutrisyncUserServiceImpl.java
+++ nutrisync-service/src/main/java/com/y421/nutrisyncservice/service/nutrisyncUser/NutrisyncUserServiceImpl.java
@@ -1,7 +1,45 @@
 package com.y421.nutrisyncservice.service.nutrisyncUser;
 
+import com.y421.nutrisyncservice.request.nutrisyncUser.LoginDto;
+import com.y421.nutrisyncservice.request.nutrisyncUser.NutrisyncUserRequestDto;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
 public class NutrisyncUserServiceImpl implements NutrisyncUserService {
+
+    @Override
+    public ResponseEntity<Object> register(NutrisyncUserRequestDto dto) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Object> login(LoginDto dto) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Object> updateProfile() {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Object> deleteAccount() {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Object> calculateBMI() {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Object> updateMetrics() {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Object> getHealthStatus() {
+        return null;
+    }
 }
