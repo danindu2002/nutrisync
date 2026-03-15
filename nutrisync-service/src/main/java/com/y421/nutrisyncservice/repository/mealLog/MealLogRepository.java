95380b093bfe67bbf7f0761c9e413b0e23535702
NirmalAnji <124679453+NirmalAnji@users.noreply.github.com>
2026-03-15 23:53:00 +0530

    Dashboard Repository Changes

--- nutrisync-service/src/main/java/com/y421/nutrisyncservice/repository/mealLog/MealLogRepository.java
+++ nutrisync-service/src/main/java/com/y421/nutrisyncservice/repository/mealLog/MealLogRepository.java
@@ -12,4 +12,14 @@ public interface MealLogRepository extends JpaRepository<MealLog,Long> {
 
     @Query("SELECT m FROM MealLog m WHERE m.user.userId = :userId AND CAST(m.createdOn AS date) = :date AND m.isDeleted = false")
     List<MealLog> findByUserIdAndDate(@Param("userId") Long userId, @Param("date") LocalDate date);
+
+
+    @Query("SELECT m FROM MealLog m " +
+            "WHERE m.user.userId = :userId " +
+            "AND CAST(m.createdOn AS date) BETWEEN :startDate AND :endDate " +
+            "AND m.isDeleted = false " +
+            "ORDER BY m.createdOn ASC")
+    List<MealLog> findByUserIdAndDateRange(@Param("userId") Long userId,
+                                           @Param("startDate") LocalDate startDate,
+                                           @Param("endDate") LocalDate endDate);
 }
